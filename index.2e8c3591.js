8722107d4d772fd8e894c9d5a03c2cf37cc65f70
S1lvyu <119692324+S1lvyu@users.noreply.github.com>
2023-04-12 18:45:27 +0000

    Deploying to gh-pages from @ S1lvyu/ice-cream-team2@331f3f4e49a28df2a5fa4a994e0db55e2f88ac44 🚀

---
+++ index.2e8c3591.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-2]"),closeModalBtn:document.querySelector("[data-modal-close-2]"),modal:document.querySelector("[data-modal-2]")};function o(){e.modal.classList.toggle("is-hidden-2")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.2e8c3591.js.map
---
+++ index.bd4bab8c.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[footer-data-modal-open-1]"),closeModalBtn:document.querySelector("[footer-data-modal-close-1]"),modal:document.querySelector("[footer-data-modal-1]")};function e(){o.modal.classList.toggle("footer-is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.bd4bab8c.js.map
