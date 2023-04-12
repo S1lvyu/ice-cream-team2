24b75ba20e8c5334260ebefb2b75d1bfbb2057e7
S1lvyu <119692324+S1lvyu@users.noreply.github.com>
2023-04-12 19:40:08 +0000

    Deploying to gh-pages from @ S1lvyu/ice-cream-team2@d1ac2aaf0bb8e73edeaa9c2ba8c340b6cbf9d08e 🚀

---
+++ index.0de6b799.js
@@ -0,0 +1,2 @@
+function howButton(){var t=document.getElementById("how-button__text");"none"===t.style.display?t.style.display="block":t.style.display="none"}
+//# sourceMappingURL=index.0de6b799.js.map
